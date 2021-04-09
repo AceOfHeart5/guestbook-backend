74f027a55422f74ef3a117bc72c5029e54e1c765
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-04-09 02:10:28 -0400

    add basic functionality

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 require('dotenv').config();
-const { config } = require('dotenv');
+const mysqlConnection = require('mysql').createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+const mysqlTable = process.env.MYSQL_TABLE;
+const mysqlColumn = process.env.MYSQL_COLUMN;
+mysqlConnection.connect();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || process.env.PORT_DEV;
+const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('This is the backend for the guestbook app.');
+    res.send('This is the guestbook app backend.');
 });
 
-app.get('/env', (req, res) => {
-    res.send(process.env);
+app.get('/printenvs', (req, res) => {
+    console.log(process.env);
+    res.send('The environment variables have been logged to the console.');
+});
+
+app.get('/getmessages', (req, res) => {
+    mysqlConnection.query(`SELECT * FROM ${mysqlTable};`, (err, rows, fields) => {
+        res.send(rows);
+    });
 })
+
+app.post('/addmessage', (req, res) => {
+    mysqlConnection.query(`INSERT INTO ${mysqlTable}(${mysqlColumn}) VALUES(?)`, 
+        [req.body.entry],
+        function(err, result) {
+            if (err === null) res.send(`added message: ${req.body.entry}`);
+            else res.send(err);
+        }
+    );
+});
 
 app.listen(port, () => {
     console.log(`Guestbook app listening at http://localhost:${port}`);
